d4dd2847ae066123ebc83f504a3a03f9330a9eba
Muhamad Yasir Noval <myasirnoval@gmail.com>
2025-07-31 13:51:20 +0700

    melanjutkan materi node js

---
+++ udemy/pzn/belajar-nodejs-dasar2/path.mjs
@@ -0,0 +1,8 @@
+import path from 'path';
+
+// fake directory
+const file = "/Users/myasirn/contoh.txt";
+
+console.info(path.dirname(file));
+console.info(path.basename(file));
+console.info(path.extname(file));
---
+++ udemy/pzn/belajar-nodejs-dasar2/web-api.js
@@ -0,0 +1,4 @@
+/*
+* statement ini tidak akan jalan jika menggunakan NodeJS
+* */
+const request = new XMLHttpRequest();
