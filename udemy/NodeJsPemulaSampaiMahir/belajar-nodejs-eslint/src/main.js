20adc201f18a5811b20b98a905ddc5026610ebb5
Muhamad Yasir Noval <myasirnoval@gmail.com>
2025-08-28 14:07:40 +0700

    belajar nodejs eslint

---
+++ udemy/NodeJsPemulaSampaiMahir/belajar-nodejs-eslint/src/main.js
@@ -0,0 +1,4 @@
+export const sayHello = (name) => {
+    const message = `Hello ${name}`;
+    console.info(`message`);
+};
---
+++ udemy/NodeJsPemulaSampaiMahir/belajar-nodejs-eslint/src/repository.js
@@ -0,0 +1,8 @@
+export async function getData() {
+    return "Hello World";
+}
+
+export function sum(a, b) {
+    const total = a + b;
+    return total;
+}
